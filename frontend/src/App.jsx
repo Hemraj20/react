a7074dca43e112c63279ffdb69252ec9ca0283e4
Hemraj20 <bhandarihemraj5@gmail.com>
2026-08-02 16:25:59 +0545

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+const App = () => {
+  return(
+    <div className="flex">
+
+    
+    <div className="h-screen flex justify-center">
+      <h1 className="text-red-400 text-7xl font-bold flex justify-center mt-2 bg-blue-50 w-80 h-40 rounded-full">Hello Everyone</h1>
+    </div>
+   
+    <div className="h-screen flex justify-center">
+      <h1 className="text-red-400 text-7xl font-bold flex justify-center mt-2 bg-blue-50 w-80 h-40 rounded-full">Hello Everyone</h1>
+    </div>
+
+    </div>
+  );
+
+};
+
+export default App
